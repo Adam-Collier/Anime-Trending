fd9e1db51edfb4718aa0cc9766855f9cff56370a
Adam Collier <adamcollierart@gmail.com>
2019-02-19 22:35:40 +0000

    add head for site meta

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,7 +1,12 @@
+import Head from "next/head";
 import Header from "./Header";
 
 const Layout = props => (
   <div>
+    <Head>
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Header />
     {props.children}
     <style jsx global>{`
