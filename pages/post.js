9293eed640bc9a1dec58949a8e229e3f309da09d
Adam Collier <adamcollierart@gmail.com>
2019-02-19 22:32:27 +0000

    get episode data and pass into EpisodeList component

--- pages/post.js
+++ pages/post.js
@@ -1,32 +1,50 @@
 import Layout from "../components/MyLayout.js";
-let fetch = require("isomorphic-unfetch");
 import axios from "axios";
 import AnimeHeader from "../components/AnimeHeader";
+import AnimeContent from "../components/AnimeContent";
+import EpisodeList from "../components/EpisodeList";
 
-const Post = ({ header }) => (
-  <Layout>
-    <AnimeHeader data={header} />
-  </Layout>
-);
+const Post = ({ header, episodes }) => {
+  console.log(episodes);
+  return (
+    <Layout>
+      <AnimeHeader data={header} />
+      <AnimeContent>
+        <EpisodeList data={episodes} />
+      </AnimeContent>
+    </Layout>
+  );
+};
 
-Post.getInitialProps = async function (context) {
+Post.getInitialProps = async function(context) {
   const { id } = context.query;
 
   async function getTrendingHeader() {
     try {
-      let trendingData = axios.get(`https://kitsu.io/api/edge/anime/${id}`)
+      let trendingData = axios.get(`https://kitsu.io/api/edge/anime/${id}`);
       let trendingHeader = await trendingData;
-
-      console.log(trendingHeader.data.data)
       return trendingHeader.data.data;
     } catch (error) {
       console.log(error);
     }
   }
 
-  return {
-    header: await getTrendingHeader()
+  async function getEpisodeList() {
+    try {
+      let data = axios.get(
+        `https://kitsu.io/api/edge/episodes?filter%5BmediaType%5D=Anime&filter%5Bmedia_id%5D=${id}&sort=number&page[limit]=20`
+      );
+      let episodeList = await data;
+      return episodeList.data.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
+
+  return {
+    header: await getTrendingHeader(),
+    episodes: await getEpisodeList()
+  };
+};
 
 export default Post;
