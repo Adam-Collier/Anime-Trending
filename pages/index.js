46e4227db3c2187ee35255b3f9a3ee676ee14c68
Adam Collier <adamcollierart@gmail.com>
2019-02-23 16:43:37 +0000

    wrapped content in AnimeContent component

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,54 @@
 import Layout from "../components/MyLayout.js";
 import axios from "axios";
 
+import AnimeContent from "../components/AnimeContent";
 import AnimeHeader from "../components/AnimeHeader";
-import AnimeContainer from "../components/AnimeContainer";
+import AnimeCategories from "../components/AnimeCategories.js";
 
 export default function Index({ header, data }) {
   return (
     <Layout>
-      <AnimeHeader data={header} />
-      <AnimeContainer data={data} />
+      <AnimeContent
+        columns="minmax(0, 1fr) 250px 700px minmax(0, 1fr)"
+        rows="auto auto auto auto"
+      >
+        <AnimeHeader data={header} />
+        <AnimeCategories data={data} />
+      </AnimeContent>
     </Layout>
   );
 }
 
-Index.getInitialProps = async function () {
+Index.getInitialProps = async function() {
   let obj = {
     comedy: 160,
     action: 150,
     fantasy: 156,
     thriller: 159,
-    adventure: 157,
-  }
+    adventure: 157
+  };
 
-  let categories = Object.keys(obj)
+  let categories = Object.keys(obj);
 
-  let promiseArray = Object.keys(obj).map((x) => axios.get(`https://kitsu.io/api/edge/trending/anime?limit=15&in_category=true&category=${obj[x]}`))
+  let promiseArray = Object.keys(obj).map(x =>
+    axios.get(
+      `https://kitsu.io/api/edge/trending/anime?limit=15&in_category=true&category=${
+        obj[x]
+      }`
+    )
+  );
 
-  let data = await Promise.all(promiseArray)
+  let data = await Promise.all(promiseArray);
 
   let categoryLists = categories.map((x, i) => {
     return { [categories[i]]: data[i].data.data };
-  })
+  });
 
   async function getTrendingHeader() {
     try {
-      let trendingData = axios.get(`https://kitsu.io/api/edge/trending/anime?limit=1`)
+      let trendingData = axios.get(
+        `https://kitsu.io/api/edge/trending/anime?limit=1`
+      );
       let trendingHeader = await trendingData;
       return trendingHeader.data.data[0];
     } catch (error) {
@@ -45,5 +59,5 @@ Index.getInitialProps = async function () {
   return {
     data: await categoryLists,
     header: await getTrendingHeader()
-  }
+  };
 };
