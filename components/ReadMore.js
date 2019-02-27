76c0d7d4dd95c407362b027eb3b07639c725b281
Adam Collier <adamcollierart@gmail.com>
2019-02-27 16:21:14 +0000

    truncate by character limit

--- components/ReadMore.js
+++ components/ReadMore.js
@@ -1,6 +1,8 @@
-import react, { useState } from "react";
+import { Fragment, useState, useRef, useEffect } from "react";
 
 const ReadMore = props => {
+  let { charLimit, data: content, height } = props;
+
   const [toggleState, setToggleState] = useState("off");
 
   function toggle() {
@@ -9,11 +11,20 @@ const ReadMore = props => {
 
   return (
     <div>
-      <div />
-      <p className={`description ${!toggleState ? "expand" : ""}`}>
-        {props.data}
-      </p>
-      <p onClick={toggle}>{toggleState ? `read more` : `read less`}</p>
+      {content.length > charLimit ? (
+        <Fragment>
+          <p className={`description ${!toggleState ? "expand" : ""}`}>
+            {!toggleState
+              ? content
+              : `${content.substr(0, content.lastIndexOf(" ", charLimit))}...`}
+          </p>
+          <p onClick={toggle}>{toggleState ? `read more` : `read less`}</p>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>{content}</p>
+        </Fragment>
+      )}
       <style jsx>{`
         p {
           color: #ffffff;
@@ -21,8 +32,6 @@ const ReadMore = props => {
         }
         .description {
           position: relative;
-          height: ${props.height ? props.height : "109px"};
-          overflow: hidden;
         }
         .description:after {
           content: "";
@@ -46,6 +55,50 @@ const ReadMore = props => {
       `}</style>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <p
+  //       className={`${!readMoreState ? "description" : ""} ${
+  //         !toggleState ? "expand" : ""
+  //       }`}
+  //       ref={textInput}
+  //     >
+  //       {content}
+  //     </p>
+  //     <p onClick={toggle}>{toggleState ? `read more` : `read less`}</p>
+  //     <style jsx>{`
+  //       p {
+  //         color: #ffffff;
+  //         cursor: pointer;
+  //       }
+  //       .description {
+  //         position: relative;
+  //         height: ${height ? height : "109px"};
+  //         overflow: hidden;
+  //       }
+  //       .description:after {
+  //         content: "";
+  //         position: absolute;
+  //         bottom: 0;
+  //         left: 0;
+  //         right: 0;
+  //         background: linear-gradient(to top, #1f202c 20%, rgba(0, 0, 0, 0));
+  //         height: 70px;
+  //       }
+  //       .expand {
+  //         height: 100%;
+  //       }
+  //       .expand:after {
+  //         background: linear-gradient(
+  //           to top,
+  //           rgba(31, 32, 41, 0) 20%,
+  //           rgba(0, 0, 0, 0)
+  //         );
+  //       }
+  //     `}</style>
+  //   </div>
+  // );
 };
 
 export default ReadMore;
