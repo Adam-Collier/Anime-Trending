76c45c97f6c5261f7c490c5de7ccfdc2f2778d0c
Adam Collier <adamcollierart@gmail.com>
2019-03-07 14:46:02 +0000

    background color change, shadow instead of hr

--- components/Tabs.js
+++ components/Tabs.js
@@ -35,18 +35,37 @@ const Tabs = props => {
       <style jsx>
         {`
           .tabs {
-            grid-row: 3/4;
+            grid-row: 4/5;
             grid-column: 3/4;
+            background: #19191f;
+            border-radius: 5px 5px 0 0;
+            position: sticky;
+            top: 10px;
           }
           .tab-list {
-            border-bottom: 1px solid #ccc;
-            padding-left: 0;
+            background: #19191f;
+            padding: 5px 0 5px 4px;
             margin-top: 0;
+            position: -webkit-sticky;
             position: sticky;
-            top: 0px;
-            background: #1f202c;
-            padding-top: 10px;
+            top: 10px;
             z-index: 2;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.4);
+          }
+          .tab-list:after {
+            content: "";
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: -10px;
+            height: 10px;
+            background: #1f202c;
+          }
+          @media (max-width: 768px) {
+            .tabs {
+              grid-row: 4/5;
+              grid-column: 1/5;
+            }
           }
         `}
       </style>
