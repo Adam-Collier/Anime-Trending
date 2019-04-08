dbf5e7c3fd19a780ec066373e7bfd8bcebfeb257
Adam Collier <adamcollierart@gmail.com>
2019-04-08 20:25:51 +0100

    compression dependency required

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
 const os = require('os')
+const compression = require('compression')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
